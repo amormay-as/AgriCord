23f77041e4d0e20fdd6ec19b94f9270f26b69174
moyazon <amormayas022@gmail.com>
2025-04-01 20:49:06 +0800

    added proper folder structure and modify some codes in LoginView

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -9,14 +9,14 @@ function onClick() {
 </script>
 
 <template>
-  <v-responsive class="border rounded" max-height="300">
+  <v-responsive class="border rounded" max-height="1000">
     <v-app :theme="theme">
-      <v-app-bar class="px-3">
+      <v-app-bar class="px-3" color="green">
         <v-spacer></v-spacer>
 
         <v-btn
           :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
+          text="Dark mode"
           slim
           @click="onClick"
         ></v-btn>
@@ -24,9 +24,29 @@ function onClick() {
 
       <v-main>
         <v-container>
-          <h1>Main Content</h1>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-card class="mx-auto" prepend-icon="$vuetify" subtitle="Record Monitoring">
+                <template v-slot:title>
+                  <span class="font-weight-black">Welcome to AgriCord</span>
+                </template>
+
+                <v-card-text class="bg-surface-light pt-4">
+                  <v-form fast-fail @submit.prevent>
+                    <v-text-field label="Email"></v-text-field>
+
+                    <v-text-field label="Password" type="password"></v-text-field>
+
+                    <v-btn class="mt-2" type="login" block>Login</v-btn>
+                  </v-form>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
         </v-container>
       </v-main>
+
+      <v-footer color="green" border app>AgriCord - 2025</v-footer>
     </v-app>
   </v-responsive>
 </template>
