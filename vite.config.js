4abd263b3476432382d1a00666081f417f2c7c89
moyazon <amormayas022@gmail.com>
2025-05-05 11:26:51 +0800

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    vue(),
+    ...(mode === 'development' ? [vueDevTools()] : []), // only load in dev
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+}))
